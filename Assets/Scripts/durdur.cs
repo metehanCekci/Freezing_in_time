a9b0a442e2315afb14f0d41716bc19fcecf6b5c6
Metehan <metehancekci9@gmail.com>
2024-12-28 08:22:17 +0300

    menu

---
+++ Assets/Scripts/durdur.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class durdur : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    public void cikis()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
